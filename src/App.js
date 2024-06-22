28666ec8508353969dab3864e697dae14faa14ec
praveen kumar <programmerpraveenkumar@gmail.com>
2024-06-22 17:41:16 +0530

    api integration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(async ()=>{
+    let res = await fetch("https://node-express-zfxd.onrender.com/",{"method":"GET"});
+    let json = await res.json();
+    console.log(json);
+  })
   return (
     <div className="App">
     <h1>this is react app</h1>
