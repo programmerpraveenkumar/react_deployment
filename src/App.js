e6707b8899c38ee39b92e14679a645b240068b0a
praveen kumar <programmerpraveenkumar@gmail.com>
2024-06-23 22:56:52 +0530

    file added

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { useEffect } from 'react';
 import { BrowserRouter, RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
+import ErrorPage from './ErrorPage';
  const router = createBrowserRouter([
   {"path":"/",element:<Home/>},
   {"path":"/home",element:<Home/>},
   {"path":"/about",element:<About/>},  
+  {"path":"*",element:<ErrorPage/>},
  ])
 
 function App() {  
